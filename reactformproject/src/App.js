7cabee32201e956c74bd7ddcc23312c433b61768
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-21 15:28:55 +0300

    created project skeleton

---
+++ reactformproject/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
