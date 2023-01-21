c7d86507f41b2f4b6ff488d2d314856ef1b31edb
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2023-01-21 17:54:33 +0300

    created footer

--- reactformproject/src/components/Footer.jsx
+++ reactformproject/src/components/Footer.jsx
@@ -1,6 +1,20 @@
+/* eslint-disable react/jsx-no-target-blank */
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <footer className="flex justify-evenly bg-[#333333] text-[#FAFAFA] py-4 ">
+        <p>
+          © {new Date().getFullYear()} Copyright - Developed by{" "}
+          <a
+            target="_blank"
+            className="text-white underline"
+            href="https://github.com/codeDiaries-devTeam"
+          >
+            codeDiaries Team
+          </a>
+        </p>
+      </footer>
+    </div>
   )
 }
 
